b3647b6cd24a7e19b4b74458a03640ee7f034085
Josh Lane <jplane@gmail.com>
2016-04-12 11:57:42 -0400

    initial add

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:60067/api/blob";    // swap with cloud URI when you deploy to Azure
+
+            var random = new Random(Environment.TickCount);
+
+            var twentyMb = 1024*1024*20;
+
+            var data = new byte[twentyMb];
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte) random.Next(0, 256);
+            }
+
+            var http = new HttpClient();
+
+            var result = http.PostAsync(uri, new ByteArrayContent(data)).Result;
+
+            Console.WriteLine(result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result);
+        }
+    }
+}
---
+++ Server/Controllers/BlobController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Server.Controllers
+{
+    public class BlobController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post()
+        {
+            try
+            {
+                //return await EchoLength();
+                return await UploadToAzureStorage(Request);
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent("Error: " + ex.Message)
+                };
+            }
+        }
+
+        private async Task<HttpResponseMessage> UploadToAzureStorage(HttpRequestMessage request)
+        {
+            var storageAccount = CloudStorageAccount.Parse("<YOUR-STORAGE-CONNECTION-STRING>");
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("tmp");
+
+            container.CreateIfNotExists();
+
+            var blob = container.GetBlockBlobReference("test");
+
+            await blob.UploadFromStreamAsync(await request.Content.ReadAsStreamAsync());
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent("done!")
+            };
+        }
+
+        private async Task<HttpResponseMessage> EchoLength()
+        {
+            var count = 0;
+
+            using (var body = await Request.Content.ReadAsStreamAsync())
+            {
+                var chunk = new byte[1024];
+                var read = await body.ReadAsync(chunk, 0, 1024);
+
+                while (read > 0)
+                {
+                    count += read;
+                    read = await body.ReadAsync(chunk, 0, 1024);
+                }
+            }
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(count.ToString())
+            };
+        }
+    }
+}
